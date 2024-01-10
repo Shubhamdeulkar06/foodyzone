ab9a8f553522dc75540913c938bb2a541a298657
Shubhamdeulkar06 <shuboy0868@gmail.com>
2024-01-10 11:27:24 +0530

    fixed cors issue

--- src/components/Body.js
+++ src/components/Body.js
@@ -16,7 +16,10 @@ const Body = () => {
 
   async function getRestaurants() {
     const data = await fetch(
-      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.org/?" +
+        encodeURIComponent(
+          "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+        )
     );
     const json = await data.json();
     console.log(json);
